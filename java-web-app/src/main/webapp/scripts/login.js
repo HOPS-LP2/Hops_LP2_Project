1d2e41a67cc6b6e2b0db72e22a6aae260b911d90
Davi Duarte <101777522+DaviJDuarte@users.noreply.github.com>
2023-09-09 18:29:56 +0000

    FIx eye icon in login form

--- java-web-app/src/main/webapp/scripts/login.js
+++ java-web-app/src/main/webapp/scripts/login.js
@@ -1,9 +1,8 @@
 const show_password = document.querySelector('.show-password');
-const show_password_img = document.querySelector('.show-password img');
 const password_input = document.querySelector('#password');
 
 show_password.addEventListener('click', () => {
     password_input.type = password_input.type === 'password' ? 'text' : 'password'
 
-    show_password_img.src = show_password_img.src.includes('open') ? '../public/icons/eye-close.png' : '../public/icons/eye-open.png'
+    show_password.src = show_password.src.includes('open') ? '../public/icons/eye-close.png' : '../public/icons/eye-open.png'
 })
