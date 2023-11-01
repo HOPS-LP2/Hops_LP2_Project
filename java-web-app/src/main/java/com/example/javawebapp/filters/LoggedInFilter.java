da22f3ab0947de7b828c28c19689d8863486d161
Davi J. Duarte <davijduarte22@gmail.com>
2023-10-31 23:06:07 -0300

    Does anyone even read these commit messages??

--- java-web-app/src/main/java/com/example/javawebapp/filters/LoggedInFilter.java
+++ java-web-app/src/main/java/com/example/javawebapp/filters/LoggedInFilter.java
@@ -12,7 +12,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-@WebFilter(value = { "/signup", "/resetPassword", "/recoverPassword" })
+@WebFilter(value = { "/login", "/signup", "/resetPassword", "/recoverPassword" })
 public class LoggedInFilter implements Filter {
 
     @Override
--- java-web-app/src/main/java/com/example/javawebapp/forms/LoginForm.java
+++ java-web-app/src/main/java/com/example/javawebapp/forms/LoginForm.java
@@ -4,12 +4,16 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public class LoginForm {
     @NotNull(message = "{email.notNull}")
-    @NotBlank
-    @Email
+    @NotBlank(message = "{email.notBlank}")
+    @Email(message = "email.notEmail")
+    @Size(min = 5, max = 100, message = "{email.size}")
     private String email;
+
+    @Size(min = 8, max = 100, message = "{password.size}")
     @NotNull(message = "{password.notNull}")
     @NotEmpty(message = "{password.notEmpty}")
     private String password;
